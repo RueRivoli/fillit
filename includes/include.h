114d2896850e7408b4792305878cc7e5278f0aa4
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2017-01-19 13:25:07 +0100

    commencement de get_tetriminos

--- includes/include.h
+++ includes/include.h
@@ -6,12 +6,27 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/19 12:14:48 by cchameyr          #+#    #+#             */
-/*   Updated: 2017/01/19 12:15:23 by cchameyr         ###   ########.fr       */
+/*   Updated: 2017/01/19 13:23:59 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _INCLUDE_H
 # define _INCLUDE_H
+
+# include "../libft/libft.h"
+# include <fcntl.h>
+
+typedef struct	s_tetris
+{
+	char				*form[4];
+	struct s_tetris		*next;
+}				t_tetris;
+
+typedef struct	s_fillit
+{
+	t_tetris	*tetri;
+}				t_fillit;
+
 
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,42 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/19 12:15:21 by cchameyr          #+#    #+#             */
-/*   Updated: 2017/01/19 12:15:50 by cchameyr         ###   ########.fr       */
+/*   Updated: 2017/01/19 13:24:36 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/include.h"
 
-int		main(int argc, char **argv)
+static int		get_tetriminos(t_fillit *fillit, char *path)
 {
+	char	*str;
+	int		fd;
+	int		i;
+
+	if ((fd = open(path, O_RDONLY)) == -1)
+		return (_ERROR_);
+	while (get_next_line(fd, &str))
+	{
 
 
+
+	}
+	return (_SUCCESS_);
+}
+
+int				main(int argc, char **argv)
+{
+	t_fillit	f;
+
+	if (argc != 2)
+		ft_putstr("usage :\t./fillit [-V --Visual] [source_file].fillit\n");
+	else
+	{
+		if (get_tetriminos(&f, argv[1]) == _ERROR_)
+			ft_putstr("Error\n");
+	}
+
+	(void)argc;
+	(void)argv;
 	return (0);
 }
