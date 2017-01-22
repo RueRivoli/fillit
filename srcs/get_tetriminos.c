b2e0c1c3b80c98ed0a795f65646b10f27e85af9b
Florent Gallois <florentgallois@MacBook-Pro-de-Florent.local>
2017-01-22 01:41:20 +0100

    quelques modif

--- srcs/get_tetriminos.c
+++ srcs/get_tetriminos.c
@@ -8,7 +8,7 @@
 /*   Created: 2017/01/20 12:22:04 by cchameyr          #+#    #+#             */
 /*   Updated: 2017/01/20 18:23:37 by fgallois         ###   ########.fr       */
 /*                                                                            */
-/* ***************************************************s*********************** */
+/* ************************************************************************** */
 
 #include "../includes/include.h"
 
@@ -19,15 +19,13 @@ t_tetris	*last_maillon(t_tetris **begin)
 	list = *begin;
 	if (!list)
 	{
-		*(begin) = ft_memalloc(sizeof(t_tetris));
+		(*begin) = ft_memalloc(sizeof(t_tetris));
 		(*begin)->next = NULL;
 	}
 	else
 	{
 		while (list->next)// list = list->next
-		{
 			list = list->next;
-		}
 		list->next = ft_memalloc(sizeof(t_tetris));
 		list = list->next;
 		list->next = NULL;
@@ -55,8 +53,7 @@ static int		create_chaine(t_tetris **begin, char *str)
 			return (_ERROR_);
 		if (i == 0 && j == 1)
 		{
-			YOLO1
-				list = last_maillon(begin);
+			list = last_maillon(begin);
 			// faire une fonction qui prend l'adresse de fillit->tetri (begin)
 			// Si arrivé dans la fonction, begin vaut NULL -> le malloc;
 			// Si arrivé dans la fonction, begin n'est pas NULL -> aller a la fin, et malloc le dernier maillon
@@ -66,9 +63,7 @@ static int		create_chaine(t_tetris **begin, char *str)
 		j = 0;
 		ft_strncpy(list->form[i++], str, 4);
 		if (i == 4)
-		{
 			i = 0;
-		}
 	}
 	return (_SUCCESS_);
 }
@@ -80,7 +75,9 @@ int				get_tetriminos(t_fillit *f, char *path)
 
 	if ((fd = open(path, O_RDONLY)) == -1)
 		return (_ERROR_);
-	//	f->tetri = ft_memalloc(sizeof(t_tetris));
+	f->tetri = ft_memalloc(sizeof(t_tetris));
+	if (f->tetri== NULL)
+		exit(0);
 	while (get_next_line(fd, &str))
 	{
 		if (create_chaine(&f->tetri, str) == _ERROR_)
