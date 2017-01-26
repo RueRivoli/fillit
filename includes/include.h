944c595ce23fe520c7a340dbe85af246e098cb38
Florent GALLOIS <fgallois@e2r9p11.42.fr>
2017-01-26 18:03:43 +0100

    modification des noms et creation des coordonnes pour chaque piece

--- includes/include.h
+++ includes/include.h
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/19 12:14:48 by cchameyr          #+#    #+#             */
-/*   Updated: 2017/01/25 15:18:02 by fgallois         ###   ########.fr       */
+/*   Updated: 2017/01/26 17:50:55 by fgallois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,14 @@ typedef struct	s_tetris
 {
 	char				form[4][4];
 	int					index;
+	int					coord_x[4];
+	int					coord_y[4];
 	struct s_tetris		*next;
 }				t_tetris;
 
 typedef struct	s_fillit
 {
-	char		array[26][26];
+	char		square[26][26];
 	int			size;
 	t_tetris	*tetri;
 }				t_fillit;
@@ -37,7 +39,9 @@ int		get_tetriminos(t_fillit *f, char *path);
 
 void	display_list(t_tetris *list);
 
-void	display_array(char array[26][26]);
+void	display_square(char square[26][26]);
+
+void	display_coord(t_tetris *list);
 
 int		check_tetriminos(t_tetris *tetri);
 
