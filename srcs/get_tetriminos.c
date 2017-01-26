97c5bfb1b737bd843ed27797efc9bcbb38f306af
Florent GALLOIS <fgallois@e2r9p11.42.fr>
2017-01-26 18:07:30 +0100

    initialisation des coordonnees des structures

--- srcs/get_tetriminos.c
+++ srcs/get_tetriminos.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/20 12:22:04 by cchameyr          #+#    #+#             */
-/*   Updated: 2017/01/25 14:46:37 by fgallois         ###   ########.fr       */
+/*   Updated: 2017/01/26 17:56:54 by fgallois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ t_tetris	*last_maillon(t_tetris **begin)
 		*begin = ft_memalloc(sizeof(t_tetris));
 		(*begin)->next = NULL;
 		(*begin)->index = 1;
+		ft_bzero((*begin)->coord_x, 4);
+		ft_bzero((*begin)->coord_y, 4);
 		return (*begin);
 	}
 	else
@@ -30,6 +32,8 @@ t_tetris	*last_maillon(t_tetris **begin)
 			list = list->next;
 		list->next = ft_memalloc(sizeof(t_tetris));
 		list->next->index = list->index + 1;
+		ft_bzero((*begin)->coord_x, 4);
+		ft_bzero((*begin)->coord_y, 4);
 		list = list->next;
 		list->next = NULL;
 	}
