ccc31c8022f3e47f2614c66ebf5db184a9121519
Florent GALLOIS <fgallois@e2r9p13.42.fr>
2017-01-30 18:55:15 +0100

    fonction qui remplit et vide le square

---
+++ srcs/fill.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fill.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgallois <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/30 18:16:48 by fgallois          #+#    #+#             */
+/*   Updated: 2017/01/30 18:35:51 by fgallois         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/include.h"
+
+int		fill(t_fillit *fillit,t_tetris *tetri, int x, int y)
+{
+	int		cpt;
+	int		fx;
+	int		fy;
+	cpt = 0;
+	while (cpt < 4)
+	{
+		fx = (tetri->coord_x[cpt] - tetri->coord_x[0]) + x;
+		fy = (tetri->coord_y[cpt] - tetri->coord_y[0]) + y;
+		if (fx < 0 || fy < 0 || fx >= fillit->size || fy >= fillit->size)
+			return (0);
+		fillit->square[fy][fx] = (char)tetri->index;
+		cpt ++;
+	}
+	return (1);
+}
+
+
+int		empty(t_fillit *fillit,t_tetris *tetri, int x, int y)
+{
+	int		cpt;
+	int		fx;
+	int		fy;
+	cpt = 0;
+	while (cpt < 4)
+	{
+		fx = (tetri->coord_x[cpt] - tetri->coord_x[0]) + x;
+		fy = (tetri->coord_y[cpt] - tetri->coord_y[0]) + y;
+		if (fx < 0 || fy < 0 || fx >= fillit->size || fy >= fillit->size)
+			return (0);
+		fillit->square[fy][fx] = '.';
+		cpt ++;
+	}
+	return (1);
+}
