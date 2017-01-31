9b3a2fae46dc4194fd135285ae494eca376d166b
Florent GALLOIS <fgallois@e2r10p16.42.fr>
2017-01-31 18:59:08 +0100

    inversions de b et a

--- srcs/recursive.c
+++ srcs/recursive.c
@@ -6,39 +6,49 @@
 /*   By: fgallois <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/30 14:24:09 by fgallois          #+#    #+#             */
-/*   Updated: 2017/01/30 18:47:59 by fgallois         ###   ########.fr       */
+/*   Updated: 2017/01/31 18:46:28 by fgallois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/include.h"
+#include <stdio.h>
 
 int		recursive(t_fillit *fillit, t_tetris *tetri)
 {
 	int x;
 	int y;
 
-	x = fillit->curr_x;
-	y = fillit->curr_y;
+	x = 0;
+	y = 0;
+	//printf("size : %d\n", fillit->size);
 	if (tetri == NULL)
 		return (1);
 	else
 	{
 		while (x < fillit->size && y < fillit->size)
 		{
-			ft_putnbr(match_in_all(fillit, tetri, x, y));
+			//ft_putnbr(match_in_all(fillit, tetri, x, y));
 			if (match_in_all(fillit, tetri, x, y))
 			{
 				int a;
 				int b;
-				YOLO
 				a = tetri->pos[0];
 				b = tetri->pos[1];
-				fill(fillit, tetri, a, b);
+				fill(fillit, tetri, b, a);
+				//ft_putstr("Field :\n");
+				//display_field(fillit);
+				if (tetri->next == NULL)
+				{	//ft_putstr("FIN\n");
+				}
 				if (recursive(fillit, tetri->next))
+				{
+					//ft_putstr("chat :\n");
+					//display_list(tetri->next);
 					return (1);
+				}
 				else
 				{
-					empty(fillit, tetri, a, b);
+					empty(fillit, tetri, b, a);
 					a = -1;
 					b = -1;
 				}
@@ -56,13 +66,9 @@ int		recursive(t_fillit *fillit, t_tetris *tetri)
 
 int		recursive_size(t_fillit *fillit, t_tetris *tetri)
 {
-	int size;
-
-	size = fillit->size;
-
-	while (size <= 26 && recursive(fillit, tetri) == 0)
-		size++;
-	if (recursive(fillit, tetri) == 1)
+	while (fillit->size <= 26 && recursive(fillit, tetri) == 0)
+		fillit->size++;
+	if (fillit->size <= 26)
 		return (1);
 	return (0);
 }
