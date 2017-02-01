e168d6d2a291a6c3256cce4cdf1ab07eff613a96
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2017-02-01 15:48:35 +0100

    deplacement de la fonction match_in_square dans le fichier match.c

--- srcs/match.c
+++ srcs/match.c
@@ -6,7 +6,7 @@
 /*   By: fgallois <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/27 14:32:57 by fgallois          #+#    #+#             */
-/*   Updated: 2017/02/01 15:16:04 by fgallois         ###   ########.fr       */
+/*   Updated: 2017/02/01 15:48:15 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,24 @@ int		same_tetrim(t_tetris *tetri1, t_tetris *tetri2)
 		gx = tetri2->coord_x[cpt] - tetri2->coord_x[0];
 		gy = tetri2->coord_y[cpt] - tetri2->coord_y[0];
 		if (fx != gx || fy != gy)
+			return (0);
+		cpt++;
+	}
+	return (1);
+}
+
+int			match_in_square(t_fillit *fillit, t_tetris *tetri, int x, int y)
+{
+	int		cpt;
+	int		fx;
+	int		fy;
+
+	cpt = 1;
+	while (cpt < 4)
+	{	
+		fx = (tetri->coord_x[cpt] - tetri->coord_x[0]) + x;
+		fy = (tetri->coord_y[cpt] - tetri->coord_y[0]) + y;
+		if (fx < 0 || fy < 0 || fx >= fillit->size || fy >= fillit->size || fillit->square[fy][fx] != '.')
 			return (0);
 		cpt++;
 	}
--- srcs/square.c
+++ srcs/square.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 13:16:02 by cchameyr          #+#    #+#             */
-/*   Updated: 2017/02/01 13:11:20 by fgallois         ###   ########.fr       */
+/*   Updated: 2017/02/01 15:48:05 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,4 @@ void			init_square(t_fillit *f)
 		ft_memset(f->square[i], '.', 26);
 }
 
-//Recherche un match a la ligne y et la colonne x
-int			match_in_square(t_fillit *fillit, t_tetris *tetri, int x, int y)
-{
-	int		cpt;
-	int		fx;
-	int		fy;
 
-	cpt = 1;
-	while (cpt < 4)
-	{	
-		fx = (tetri->coord_x[cpt] - tetri->coord_x[0]) + x;
-		fy = (tetri->coord_y[cpt] - tetri->coord_y[0]) + y;
-		if (fx < 0 || fy < 0 || fx >= fillit->size || fy >= fillit->size || fillit->square[fy][fx] != '.')
-			return (0);
-		cpt++;
-	}
-	return (1);
-}
